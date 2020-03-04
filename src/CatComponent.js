580fe45f14110b93053cfa0d8c8dfa8b5cd0c4a6
manel turki <menelturki@gmail.com>
2020-03-04 10:23:12 +0000

    Automatically backed up by Learn

--- src/CatComponent.js
+++ src/CatComponent.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 
-export default class CatComponent extends Component {
-  render() {
-    return (
-      <div className="bar" id="cat">
-        <img src="/cat.gif" />
-      </div>
-    );
-  }
-}
+import CatComponent from './CatComponent'
+import GraceHopperQuoteComponent from './GraceHopperQuoteComponent'
+import MouseComponent from './MouseComponent'
+import EinsteinQuoteComponent from './EinsteinQuoteComponent'
+class App extends Component {
+	render() {
+		// your code in the return statement below!
+		return (
+			<div className="App">
+				<CatComponent />
+				<GraceHopperQuoteComponent />
+				<EinsteinQuoteComponent />				
+				<MouseComponent />
+				{/* one more component missing */}
+			</div>
+		);
